6bde2cea29f46464e514199d6c6d8a1cedf2fe2e
leforyou <1147724499@qq.com>
2019-01-20 17:14:02 +0800

    webpack

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # webpack
 webpack配置多页面项目
 
-PS:之前在项目中一直用gulp，以前足够应付开发。现在想换webpack配置开发多页面应用，过程踩了不坑，也学习了不少东西。
+PS:之前在项目中一直用gulp，以前足够应付开发。现在想换webpack配置开发多页面应用，过程踩了不坑，也学习了不少东西。这个webpack配置还有值得优化的地方，如：Vue2.*那样分为生产环境和开发环境两个版本的webpack配置，公共部分用webpack-merge进行合并。在开发环境不需要压缩js、css，这样获取更高的性能。Vue2.**【webpack.base.conf.js公共部分配置】【webpack.dev.conf.js开发环境】【webpack.prod.conf.js生产环境】
 
 打包：npm run build
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,14 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');//生成HTML文件
 const CleanWebpackPlugin = require('clean-webpack-plugin');//清空打包目录
-const ExtractTextPlugin = require('extract-text-webpack-plugin');//打包css
+const ExtractTextPlugin = require('extract-text-webpack-plugin');//打包css(合并成一个)，vue也是将所有css打包在一个文件，可以利用浏览器css缓存（可以加hash清除缓存）
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");//压缩并清除css中注释   npm install --save-dev optimize-css-assets-webpack-plugin
 const CopyWebpackPlugin = require('copy-webpack-plugin');//复制静态资源(图片等)
 
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');//压缩js。安装npm install uglifyjs-webpack-plugin --save-dev
+
 function resolve(dir) {
-  return path.join(__dirname, '..', dir)
+    return path.join(__dirname, '..', dir)
 }
 
 
@@ -21,7 +24,7 @@ module.exports = {
         resolve("src/page/index/index.js"),
         //"./src/page/login/index.js"
     ],*/
-    entry:{
+    entry: {
         index: path.resolve(__dirname, 'src/page/index/index.js'), //resolve("src/page/index/index.js")
         login: path.resolve(__dirname, 'src/page/login/index.js')
     },
@@ -30,15 +33,15 @@ module.exports = {
         filename: "[name].js", 
         publicPath: "/public/", 
     },*/
-    output:{
-        path:path.resolve(__dirname,"dist"),
-        filename:'js/bundle-[name].js', //例如：index.js将打包成bundle-index.js。还有其它写法"[name].js"。
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: 'js/bundle-[name].js', //例如：index.js将打包成bundle-index.js。还有其它写法"[name].js"。
     },
-    devServer:{
+    devServer: {
         //webpack-dev-server：会实时打包，但是并不会在本地硬盘生成dist目录文件，这是因为webpack-dev-server将打包好的文件放在了内存中（依旧是可以访问的），运行npm run build才会本地生成dist文件目录
         //"progress 显示打包进度 ; colors配置打包输出颜色显示  ;; -d 是debug模式，输入一个source-map，并且可以看到每一个打包的文件 ; -p 是对代码进行压缩 ; port 3008 服务器端口 ; open 启动后默认打开浏览器"
-        contentBase:path.resolve(__dirname,"dist/"), //最好设置成绝对路径（启动服务器访问的页面）
-        publicPath:'/',//静态文件
+        contentBase: path.resolve(__dirname, "dist"), //最好设置成绝对路径（启动服务器访问的页面）
+        publicPath: '/',//静态文件
         historyApiFallback: false,
         hot: true,// 开启热更新(热加载)
         inline: true,// inline 是刷新后的代码自动注入到打包后的文件中(当源文件改变时会自动刷新页面)
@@ -46,24 +49,24 @@ module.exports = {
         host: 'localhost',//localhost 或 0.0.0.0 或 127.0.0.1 或 局域网IP
         port: 8000,//服务器端口
         overlay: true,// 浏览器页面上显示错误
-        open:true //启动后默认打开浏览器
+        open: true //启动后默认打开浏览器
     },
-    module:{
+    module: {
         rules: [
             // 处理 CSS 文件的 loader
-            { 
-                test: /\.css$/, 
+            {
+                test: /\.css$/,
                 /*use: ['style-loader', 'css-loader']   //不分离css,直接打包在html内*/
                 use: ExtractTextPlugin.extract({
                     fallback: "style-loader",
-                    use: ["css-loader", "sass-loader"],
+                    use: ["css-loader"],
                     // css中的基础路径
                     publicPath: "../"
                 })
-            }, 
+            },
             // 处理 scss 文件的 loader
             {
-                test: /\.(scss|sass)$/,
+                test: /\.(scss|sass)$/, ///\.(sa|sc|c)ss$/
                 /*//不分离css,直接打包在html内
                 use: [
                     "style-loader", //将 JS 字符串生成为 style 节点 creates style nodes from JS strings
@@ -82,26 +85,33 @@ module.exports = {
             // 处理 less 文件的 loader
             {
                 test: /\.less$/,
-                use: ['style-loader', 'css-loader', 'less-loader']
-            }, 
+                //use: ['style-loader', 'css-loader', 'less-loader']
+                // 此处为分离css的写法
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: ["css-loader", "less-loader"],
+                    // css中的基础路径
+                    publicPath: "../"
+                })
+            },
             {
                 test: /\.(png|jpe?g|gif|svg)$/i,
                 use: [
-                  {
-                    loader: 'url-loader',
-                    options: {
-                      limit: 10000
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10000
+                        }
                     }
-                  }
                 ]
             },
             {
                 test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,//resolve("node_modules")
+                exclude: /(node_modules|bower_components)/,//resolve("node_modules")  排除文件夹
                 use: {
                     loader: 'babel-loader',
                     options: {
-                    presets: ['@babel/preset-env']
+                        presets: ['@babel/preset-env']
                     }
                 }
             }
@@ -109,12 +119,6 @@ module.exports = {
         ]
     },
     plugins: [
-        new ExtractTextPlugin("css/style.css"),/**/
-        /*new ExtractTextPlugin("css/[name].css"),*/
-        /*new ExtractTextPlugin({
-            filename: 'css/[name].[hash:8].min.css',
-            allChunks: true // 一开始所有css都打包
-        }),*/
         //清空打包目录dist
         new CleanWebpackPlugin(['dist'], {
             root: path.resolve(__dirname), //根目录
@@ -124,23 +128,45 @@ module.exports = {
             // Default: false - remove files
             dry: false,
         }),
+        new ExtractTextPlugin("css/style.[hash:8].min.css"),/*所有样式打包在style.css文件内*/
+        /*new ExtractTextPlugin("css/[name].css"),*/
+        /*new ExtractTextPlugin({
+            //filename: 'css/[name].[hash:8].min.css',
+            //allChunks: false, // 一开始所有css都打包
+        }),*/
+        //压缩css
+        new OptimizeCSSAssetsPlugin({
+            assetNameRegExp: /\.css$/g,
+            cssProcessor: require('cssnano'), //npm install --save-dev cssnano
+            cssProcessorPluginOptions: {
+                preset: ['default', { discardComments: { removeAll: true } }],
+            },
+            canPrint: true
+        }),
+        //压缩js
+        new UglifyJsPlugin({
+            test: /\.js(\?.*)?$/i,//默认匹配写法
+            exclude: /\node_modules/,//排除
+            sourceMap: true,
+        }),
         //复制静态资源
         new CopyWebpackPlugin([
             {
-                from: 'public',
-                to: undefined
+                from: 'public',//文件夹
+                to: undefined //默认值：复制到dist的根目录下
             },
             {
-                from: 'src/assets',
-                to: undefined
+                from: 'src/assets',//文件夹
+                to: undefined,
+                ignore: ['*.scss', '*.sass', '*.less'] //静态资源的过滤，忽略这些文件不打包到dist内
             }
         ]),
         new HtmlWebpackPlugin({
-            template:`src/page/index/index.html`,
-            filename:`html/index.html`,// 生成文件名字
+            template: `src/page/index/index.html`,
+            filename: `html/index.html`,// 生成文件名字
             // inject: false,    // 不把生成的css，js插入到html中
-            hash:false,
-            chunks:["index"],
+            hash: false,
+            chunks: ["index"],
             minify: {// 压缩html
                 removeComments: true,
                 collapseWhitespace: true,
@@ -149,11 +175,11 @@ module.exports = {
             chunksSortMode: 'dependency'
         }),
         new HtmlWebpackPlugin({
-            template:`src/page/login/index.html`,
-            filename:`html/login.html`,// 生成文件名字
+            template: `src/page/login/index.html`,
+            filename: `html/login.html`,// 生成文件名字
             // inject: false,    // 不把生成的css，js插入到html中
-            hash:false,
-            chunks:["login"],
+            hash: false,
+            chunks: ["login"],
             minify: {// 压缩html
                 removeComments: true,
                 collapseWhitespace: true,
@@ -170,5 +196,12 @@ module.exports = {
                 collapseWhitespace: true
             }
           })*/
-    ]
+    ],
+    //最佳化，最优化
+    /*optimization: {
+        //最小值
+        minimize: [
+            
+        ]
+    }*/
 }
